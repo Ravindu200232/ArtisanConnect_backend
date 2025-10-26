1ce785e234aa0e85395ea2a16482b3d51fb052f6
Ravindu Bandara Subasinha <it22098450@my.sliit.lk>
2025-10-26 06:06:44 +0530

    asd

--- controllers/InquiryController.js
+++ controllers/InquiryController.js
@@ -1,175 +1,181 @@
-
 import Inquiry from "../models/inquiry.js";
-import { checkAdmin, checkCustomer, checkOwner } from "./authController.js";
+import { checkAdmin, checkCustomer } from "./authController.js";
 
-
-export async function addInquiry(req,res) {
-
-    try{
-        if(req.user == null){
+export async function addInquiry(req, res) {
+    try {
+        if (req.user == null) {
             res.status(401).json({
-                message : "Please login and try again"
-            })
-            return  
+                message: "Please login and try again"
+            });
+            return;
         }
-        
-            const data = req.body;
-            
-            data.email = req.user.email
-            data.phone = req.user.phone
-//generate id
-            let id = 0;
 
-            const inquiryIds = await Inquiry.find().sort({id : -1}).limit(1)
-
-            if(inquiryIds.length == 0){
-                id = 1;
-            }else{
-                inquiryIds[0].id++;
-                id = inquiryIds[0].id;
-                
-            }
-            data.id = id;
-            console.log(id);
-
-            const newInquiry = new Inquiry(data);
-
-            await newInquiry.save();
-            res.json({
-                message : "Inquiry added successfully"
-            })
-            return
-
-
-        
-
-    }catch(err){
-        console.log(err)
-        res.status(500).json({
-            message : "Inquiry addedd unsuccessfully"
-        })
-    }
-    
-}
-
-export async function getInquiry(req,res){
-
-    try{
-       
-        if(checkAdmin(req)){
-            const inquiry = await Inquiry.find();
-            res.json(inquiry);
-            return
-        }
-        
-        else if(checkCustomer(req)){
-            
-            
-            const email = req.user.email
-            const inquiry = await Inquiry.find({
-                email : email
-            })
-            res.json(inquiry);
-            return
-        }
-        else{
-            res.json({
-                message : "you are not authorized to perform this action"
-            })
-        }
-    }catch(err){
-        res.status(500).json({
-            message : "can't show details"
-        })
-    }
-}
-
-export async function deleteInquiry(req,res){
-
-    try{
-        if(checkAdmin(req)){
-            const id = req.params.id;
-            await Inquiry.deleteOne({
-                _id:id
-            })
-            res.json({
-                message : "Inquiry deleted successfully"
-            })
-            return
-        }
-        else if(checkCustomer(req)){
-            const id = req.params.id;
-            const inquiry = await Inquiry.findOne({_id:id});
-            if(inquiry == null){
-                res.status(404).json({
-                    message : "Inqury not found"
-                })
-                return
-            }
-            if(inquiry.email == req.user.email){
-                    await inquiry.deleteOne({_id:id})
-                    res.json({
-                        message : "Inquiry deleted successfully"
-                    })
-                    return
-                }else{
-
-                    res.json("You are not authorized to perform this action")
-                    return
-                }
-
-            
-        }
-        else{
-            res.status(403).json({
-                message : "You are not authorized to perform this action"
-            })
-            return
-        }
-    }catch(err){
-        res.status(500).json({
-            message : "Failed to delete inquiry"
-        })
-    }
-}
-
-export async function updateInquiry(req,res){
-
-    try{
         const data = req.body;
-        const id = req.params.id
-        if(checkAdmin(req)){
-            await Inquiry.updateOne({_id:id},data)
-            res.json({
-                message : "Inquiry updated successfully"
-            })
-            return
+        
+        data.email = req.user.email;
+        data.phone = req.user.phone;
+
+        // Generate unique inquiryId
+        let inquiryId = 1;
+
+        const lastInquiry = await Inquiry.findOne().sort({ inquiryId: -1 }).limit(1);
+
+        if (lastInquiry && lastInquiry.inquiryId) {
+            inquiryId = lastInquiry.inquiryId + 1;
         }
-        else if(checkCustomer(req)){
-            const inquiry = await Inquiry.findOne({_id:id})
-            console.log(req.user.email)
-            console.log(inquiry.email)
-            if(inquiry.email == req.user.email){
-                await Inquiry.updateOne({_id:id},{message : data.message})
-                res.json({message : "Inquiry update successfully"})
-                return
-            }
-            else{
-                res.status(401).json({
-                     message : "You are not authorized to perform this action"
-                })
-                return
-            }
-        }else{
-            res.status(401).json({
-                 message : "You are not authorized to perform this action"
-            })
-            return
+
+        data.inquiryId = inquiryId;
+
+        const newInquiry = new Inquiry(data);
+        await newInquiry.save();
+
+        res.json({
+            message: "Inquiry added successfully",
+            inquiry: newInquiry
+        });
+
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Inquiry added unsuccessfully",
+            error: err.message
+        });
+    }
+}
+
+export async function getInquiry(req, res) {
+    try {
+        if (checkAdmin(req)) {
+            const inquiries = await Inquiry.find().sort({ inquiryId: -1 });
+            res.json(inquiries);
+            return;
         }
         
-    }catch(err){
+        if (checkCustomer(req)) {
+            const email = req.user.email;
+            const inquiries = await Inquiry.find({ email: email }).sort({ inquiryId: -1 });
+            res.json(inquiries);
+            return;
+        }
+        
+        res.status(403).json({
+            message: "You are not authorized to perform this action"
+        });
+        
+    } catch (err) {
+        console.log(err);
         res.status(500).json({
-            message : "Failed to update inquiry"
-        })
+            message: "Can't show details",
+            error: err.message
+        });
+    }
+}
+
+export async function deleteInquiry(req, res) {
+    try {
+        const id = req.params.id;
+
+        if (checkAdmin(req)) {
+            const result = await Inquiry.deleteOne({ _id: id });
+            
+            if (result.deletedCount === 0) {
+                res.status(404).json({
+                    message: "Inquiry not found"
+                });
+                return;
+            }
+            
+            res.json({
+                message: "Inquiry deleted successfully"
+            });
+            return;
+        }
+        
+        if (checkCustomer(req)) {
+            const inquiry = await Inquiry.findOne({ _id: id });
+            
+            if (inquiry == null) {
+                res.status(404).json({
+                    message: "Inquiry not found"
+                });
+                return;
+            }
+            
+            if (inquiry.email !== req.user.email) {
+                res.status(403).json({
+                    message: "You are not authorized to perform this action"
+                });
+                return;
+            }
+            
+            await Inquiry.deleteOne({ _id: id });
+            res.json({
+                message: "Inquiry deleted successfully"
+            });
+            return;
+        }
+        
+        res.status(403).json({
+            message: "You are not authorized to perform this action"
+        });
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Failed to delete inquiry",
+            error: err.message
+        });
+    }
+}
+
+export async function updateInquiry(req, res) {
+    try {
+        const data = req.body;
+        const id = req.params.id;
+
+        // Check if inquiry exists
+        const inquiry = await Inquiry.findOne({ _id: id });
+        
+        if (!inquiry) {
+            res.status(404).json({
+                message: "Inquiry not found"
+            });
+            return;
+        }
+
+        if (checkAdmin(req)) {
+            await Inquiry.updateOne({ _id: id }, data);
+            res.json({
+                message: "Inquiry updated successfully"
+            });
+            return;
+        }
+        
+        if (checkCustomer(req)) {
+            if (inquiry.email !== req.user.email) {
+                res.status(403).json({
+                    message: "You are not authorized to perform this action"
+                });
+                return;
+            }
+            
+            // Customers can only update message
+            await Inquiry.updateOne({ _id: id }, { message: data.message });
+            res.json({
+                message: "Inquiry updated successfully"
+            });
+            return;
+        }
+        
+        res.status(403).json({
+            message: "You are not authorized to perform this action"
+        });
+        
+    } catch (err) {
+        console.log(err);
+        res.status(500).json({
+            message: "Failed to update inquiry",
+            error: err.message
+        });
     }
 }
